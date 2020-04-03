1aa085e6373cae1881f01c2d026c0987db941b31
Sara Bertran de Lis <sarabertrandelis@gmail.com>
2020-04-03 10:36:38 -0400

    Removed iplot calls

--- code/Covid-19_v1.py
+++ code/Covid-19_v1.py
@@ -6,15 +6,19 @@
 
 import pandas as pd
 import numpy as np
+import datetime
+#import reverse_geocode
 import plotly
 import plotly.graph_objs as go
-pd.set_option('display.max_columns', 500)
-plotly.offline.init_notebook_mode(connected=True)
+
+#pd.set_option('display.max_columns', 500)
+#plotly.offline.init_notebook_mode(connected=True)
 
 
 # In[2]:
 
 
+# Load data
 df_confirmed = pd.read_csv('https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv')
 df_deaths = pd.read_csv('https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_deaths_global.csv')
 
@@ -22,7 +26,7 @@ df_deaths = pd.read_csv('https://raw.githubusercontent.com/CSSEGISandData/COVID-
 # In[3]:
 
 
-#convert table
+# Convert table
 def converttable(df):
     cols=df.columns.tolist()
     pd_list=[]
@@ -37,23 +41,19 @@ def converttable(df):
     df_new=pd.concat(pd_list,axis=0,ignore_index=True)
     return df_new
 
-
-# In[4]:
-
-
 df_confirmed_new = converttable(df_confirmed)
 df_deaths_new = converttable(df_deaths)
 
 
-# In[5]:
+# In[4]:
 
 
-#dt format
+# Change dates format
 df_confirmed_new['dt']=pd.to_datetime(df_confirmed_new['dt'])
 df_deaths_new['dt']=pd.to_datetime(df_deaths_new['dt'])
 
 
-# In[6]:
+# In[5]:
 
 
 # Rename cols
@@ -61,7 +61,7 @@ df_confirmed_new.rename(columns = {'value': 'confirmed'}, inplace = True)
 df_deaths_new.rename(columns = {'value': 'deaths'}, inplace = True)
 
 
-# In[7]:
+# In[6]:
 
 
 # Merge two tables
@@ -72,21 +72,21 @@ df_merged = df_merged[['Province/State', 'Country/Region', 'Lat', 'Long', 'dt', 
 #df_merged.head()
 
 
-# In[8]:
+# In[7]:
 
 
 # Extract and aggregate data for countries with provinces
 
 # US
-df_us = df_merged[df_merged['Country/Region'] == 'US'].copy()
-df_merged.drop(index = df_us.index, inplace = True)
-us_agg = df_us.groupby(by = 'dt').sum().copy()
-us_agg.reset_index(inplace = True)
-us_agg['Lat'] = 37.0902
-us_agg['Long'] = -95.7129
-us_agg['Province/State'] = np.nan
-us_agg['Country/Region'] = 'US'
-df_merged = pd.concat([df_merged, us_agg], sort = False, ignore_index = True)
+#df_us = df_merged[df_merged['Country/Region'] == 'US'].copy()
+#df_merged.drop(index = df_us.index, inplace = True)
+#us_agg = df_us.groupby(by = 'dt').sum().copy()
+#us_agg.reset_index(inplace = True)
+#us_agg['Lat'] = 37.0902
+#us_agg['Long'] = -95.7129
+#us_agg['Province/State'] = np.nan
+#us_agg['Country/Region'] = 'US'
+#df_merged = pd.concat([df_merged, us_agg], sort = False, ignore_index = True)
 
 # China
 df_china = df_merged[df_merged['Country/Region'] == 'China'].copy()
@@ -169,14 +169,7 @@ df_merged = pd.concat([df_merged, net_agg], sort = False, ignore_index = True)
 df_merged.drop(columns = ['Province/State'], inplace = True)
 
 
-# In[9]:
-
-
-# Mortality rate
-df_merged['MortalityRate'] = df_merged['deaths']/df_merged['confirmed']
-
-
-# In[10]:
+# In[8]:
 
 
 # Days since
@@ -214,7 +207,6 @@ df_first = pd.DataFrame(data = {'Country/Region': country_list,
 df_first['first_confirmed'] = pd.to_datetime(df_first['first_confirmed'])
 df_first['first_50confirmed'] = pd.to_datetime(df_first['first_50confirmed'])
 df_first['first_10deaths'] = pd.to_datetime(df_first['first_10deaths'])
-
 #df_first.head()
 
 # Merge firsts with main table
@@ -236,32 +228,29 @@ df_merged['days_since_10th_deaths'] = df_merged['days_since_10th_deaths'].dt.day
 # Drop intermediate columns
 #df_merged.drop(columns = ['first_confirmed', 'first_50confirmed', 'first_10deaths'], inplace = True)
 
+# Check
 #df_merged[df_merged['Country/Region'] == 'Germany'][['dt', 'confirmed', 'deaths', 'days_since_1st_conf',
 #                                                     'days_since_50th_conf', 'days_since_10th_deaths']]
 
 
-# In[11]:
+# In[9]:
 
 
-# New cases
+# Daily new cases (confirmed and deaths)
 
 # Sort values
 df_merged.sort_values(by = ['Country/Region', 'dt'], ascending = True, inplace = True)
-
 # Differences
 df_merged['confirmed_newcases'] = df_merged.groupby(by = ['Country/Region']).confirmed.diff()
 df_merged['deaths_newcases'] = df_merged.groupby(by = ['Country/Region']).deaths.diff()
-
 #df_merged[df_merged['Country/Region'] == 'Germany'][['dt', 'confirmed', 'confirmed_diff']]
 
+# Moving average
 
-# In[12]:
-
-
-# New cases moving average
+# Create columns
 df_merged['confirmed_newcases_movavg'] = np.nan
 df_merged['deaths_newcases_movavg'] = np.nan
-
+# Number of days
 days_num = df_merged['dt'].unique().shape[0]
 
 for c in country_list:
@@ -288,35 +277,12 @@ for c in country_list:
 #                                                     'confirmed_newcases_growth_movavg']]
 
 
-# In[13]:
-
-
-# Open population table
-df_pop = pd.read_csv('../data_tables/world_pop_by_country.csv')
-df_pop.rename(columns = {'Country Name': 'Country/Region', '2018': 'population_2018'}, inplace = True)
-
-# Merge population table
-df_merged = df_merged.merge(df_pop, on = 'Country/Region', how = 'left')
-
-#df_merged.sort_values('dt').head()
-
-
-# In[14]:
-
-
-# Cases by 100.000 hab.
-df_merged['confirmed_by100000pop'] = df_merged['confirmed']*100000/df_merged['population_2018']
-df_merged['deaths_by100000pop'] = df_merged['deaths']*100000/df_merged['population_2018']
-
-# Drop intermediate columns
-df_merged.drop(columns = ['Country Code', 'population_2018'], inplace = True)
-
-
-# In[15]:
+# In[10]:
 
 
 # New cases growth (new cases today divided by new cases yesterday)
 
+# Create columns
 df_merged['confirmed_newcases_growth'] = np.nan
 df_merged['deaths_newcases_growth'] = np.nan
 
@@ -339,10 +305,6 @@ for c in country_list:
         
 #df_merged[df_merged['Country/Region'] == 'Germany'][['dt', 'confirmed', 'confirmed_newcases', 
 #                                                     'confirmed_newcases_growth']]
-
-
-# In[16]:
-
 
 # New cases growth moving average
 
@@ -375,7 +337,39 @@ for c in country_list:
 #                                                     'confirmed_newcases_growth_movavg']]
 
 
-# In[17]:
+# In[11]:
+
+
+# Open population table
+df_pop = pd.read_csv('../data_tables/world_pop_by_country.csv')
+df_pop.rename(columns = {'Country Name': 'Country/Region', '2018': 'population_2018'}, inplace = True)
+
+# Merge population table
+df_merged = df_merged.merge(df_pop, on = 'Country/Region', how = 'left')
+
+#df_merged.sort_values('dt').head()
+
+
+# In[12]:
+
+
+# Cases by 100.000 hab.
+df_merged['confirmed_by100000pop'] = df_merged['confirmed']*100000/df_merged['population_2018']
+df_merged['deaths_by100000pop'] = df_merged['deaths']*100000/df_merged['population_2018']
+
+# Drop intermediate columns
+#df_merged.drop(columns = ['Country Code', 'population_2018'], inplace = True)
+df_merged.drop(columns = ['Country Code'], inplace = True)
+
+
+# In[13]:
+
+
+# Mortality rate
+df_merged['MortalityRate'] = df_merged['deaths']/df_merged['confirmed']
+
+
+# In[14]:
 
 
 # New cases by population
@@ -403,18 +397,7 @@ for c in country_list:
             tmp.loc[i+1, 'confirmed_newcases_by100000pop'] )/3) 
 
 
-# In[18]:
-
-
-# Select data for plotting
-
-# Data for the top 10 countries in number of confirmed cases
-last_date = df_merged['dt'].max()
-top10 = df_merged[df_merged['dt'] == last_date].sort_values(by = 'deaths', ascending = False).iloc[0:10, :] 
-top10_country = top10['Country/Region'].values
-
-
-# In[19]:
+# In[15]:
 
 
 # Plotting colors
@@ -428,18 +411,49 @@ two_color_a = '#002D72'
 two_color_b = '#009B77'
 
 # Plotting fonts & sizes
-#title_font = 'PT Sans Narrow'
-#subtitle_font = 'PT Sans Narrow'
-#label_font = 'PT Sans'
-title_font = 'Open Sans'
-subtitle_font = 'Open Sans'
-label_font = 'Open Sans'
+title_font = 'Gentona, Tahoma, sans-serif'
+subtitle_font = 'Gentona, Tahoma, sans-serif'
+label_font = 'Gentona, Tahoma, sans-serif'
 title_size = 24
 subtitle_size = 18
 label_size = 12
+label_size_small = 8
+
+# Graph sizes
+width_px = 720
+height_px = 450
+wide_px_small = 360
+height_px_small = 275
+
+# Line size
+line_width = 2 
+
+# Margins
+margin_t = 20
+margin_b = 150
+margin_r = 20
+margin_l = 50
+
+# Ticks
+tick_lenght = 12
+tick_col = '#eee'
 
 
-# In[20]:
+# ## <span style="color:orange">New cases</span>
+# <hr style="border: 1px solid #D3D3D3" >
+
+# In[16]:
+
+
+# Select data for plotting
+
+# Data for the top 10 countries in number of confirmed cases
+last_date = df_merged['dt'].max()
+top10 = df_merged[df_merged['dt'] == last_date].sort_values(by = 'deaths', ascending = False).iloc[0:10, :] 
+top10_country = top10['Country/Region'].values
+
+
+# In[17]:
 
 
 # Plot new cases top 10 countries
@@ -449,9 +463,9 @@ for i, c in enumerate(top10_country):
     nc = df_merged[df_merged['Country/Region'] == c].confirmed_newcases_movavg
     data.append(go.Scatter(x = df_merged[df_merged['Country/Region'] == c].dt,
                            y = nc,
-                           name = c+'     ',
+                           name = c+'  ',
                            marker = dict(color = top10_col[i]),
-                           line = dict(width = 3),
+                           line = dict(width = line_width),
                            hoverinfo = 'text',
                            hovertext = [c+':<br>{:.0f}'.format(i) for i in nc],
                            hoverlabel = dict(bordercolor = top10_col[i], 
@@ -462,13 +476,11 @@ for i, c in enumerate(top10_country):
                )
     
     
-lay = go.Layout(width = 800, 
-                height = 600, 
-                #bargap = 0.2,
-                xaxis = dict(nticks = 10,
-                             ticks = 'inside',
-                             ticklen = 12,
-                             tickcolor = '#eee',
+lay = go.Layout(width = width_px, 
+                height = height_px, 
+                xaxis = dict(ticks = 'inside',
+                             ticklen = tick_lenght,
+                             tickcolor = tick_col,
                              rangemode = 'nonnegative',
                              zeroline = False,
                              showgrid = False,
@@ -482,41 +494,19 @@ lay = go.Layout(width = 800,
                             color = label_col,
                            ),
                 legend = dict(x = 0.0, 
-                              y = -0.18,
+                              y = -0.20,
                               orientation = 'h',
                              ),
-                margin=dict(l=50, r=20, b=150, t=100, pad=0),
+                margin=dict(l = margin_l, r = margin_r, b = margin_b, t = margin_t, pad=0),
                 annotations=[dict(x = 0.0,
-                                  y = 1.2,
-                                  showarrow = False,
-                                  text = 'Daily confirmed new cases (5-day moving average)',
-                                  xref = 'paper',
-                                  yref = 'paper',
-                                  font=dict(
-                                      family = title_font,
-                                      size = title_size,
-                                      color = title_col,),
-                                 ),
-                             dict(x = 0.0,
-                                  y = 1.1,
-                                  showarrow = False,
-                                  text = 'Outbreak evolution for the current 10 most affected countries',
-                                  xref = 'paper',
-                                  yref = 'paper',
-                                  font=dict(
-                                      family = subtitle_font,
-                                      size = subtitle_size,
-                                      color = subtitle_col,),
-                                 ),
-                             dict(x = 0.0,
-                                  y = -0.18,
+                                  y = -0.20,
                                   showarrow = False,
                                   text = 'Click any country below to hide/show from the graph:',
                                   xref = 'paper',
                                   yref = 'paper',
                                   font=dict(
                                       family = label_font,
-                                      size = 12,
+                                      size = label_size,
                                       color = 'silver',),
                                  ),
                             ],
@@ -536,14 +526,96 @@ plot = plotly.offline.plot({'data':data,
                                          responsive = True)
                           )
 path = '../visuals/new_cases/'
-out_file = open(path+'timeline_newcases_date_all.html', 'w')
+out_file = open(path+'timeline_newcases_date_all_720.html', 'w')
 out_file.write('<!DOCTYPE html><html><head><script src="https://cdn.plot.ly/plotly-latest.min.js"></script></head><body>')
 out_file.write(plot)
 out_file.write('</body><html>')
 out_file.close()
 
 
-# In[21]:
+# In[18]:
+
+
+# Plot new cases top 10 countries 360px
+data = []
+
+for i, c in enumerate(top10_country):
+    nc = df_merged[df_merged['Country/Region'] == c].confirmed_newcases_movavg
+    data.append(go.Scatter(x = df_merged[df_merged['Country/Region'] == c].dt,
+                           y = nc,
+                           name = c+'  ',
+                           marker = dict(color = top10_col[i]),
+                           line = dict(width = 1.5),
+                           hoverinfo = 'text',
+                           hovertext = [c+':<br>{:.0f}'.format(i) for i in nc],
+                           hoverlabel = dict(bordercolor = top10_col[i], 
+                                             bgcolor = 'white',
+                                             font = dict(color = top10_col[i])),
+                           showlegend = True
+                          )
+               )
+    
+    
+lay = go.Layout(width = 360, 
+                height = 275, 
+                #bargap = 0.2,
+                xaxis = dict(#nticks = 10,
+                             ticks = 'inside',
+                             ticklen = 6,
+                             tickcolor = '#eee',
+                             rangemode = 'nonnegative',
+                             zeroline = False,
+                             showgrid = False,
+                            ),
+                yaxis = dict(title='Confirmed new cases',
+                             type = 'linear',
+                            ),
+                hovermode = 'closest',
+                font = dict(size = label_size_small,
+                            family = label_font,
+                            color = label_col,
+                           ),
+                legend = dict(x = -0.05, 
+                              y = -0.22,
+                              orientation = 'h',
+                             ),
+                margin=dict(l=30, r=10, b=0, t=10, pad=0),
+                annotations=[dict(x = -0.03,
+                                  y = -0.25,
+                                  showarrow = False,
+                                  text = 'Click any country below to hide/show from the graph:',
+                                  xref = 'paper',
+                                  yref = 'paper',
+                                  font=dict(
+                                      family = label_font,
+                                      size = label_size_small,
+                                      color = 'silver',),
+                                 ),
+                            ],
+               )
+
+fig = dict(data=data, layout=lay)
+#plotly.offline.iplot(fig)
+
+# Save JS
+plot = plotly.offline.plot({'data':data,
+                            'layout':lay},
+                           include_plotlyjs = False,
+                           output_type = 'div',
+                           config = dict(showLink = False,
+                                         modeBarButtonsToRemove = ['sendDataToCloud'],
+                                         displaylogo = False,
+                                         responsive = True)
+                          )
+path = '../visuals/new_cases/'
+out_file = open(path+'timeline_newcases_date_all_360.html', 'w')
+out_file.write('<!DOCTYPE html><html><head><script src="https://cdn.plot.ly/plotly-latest.min.js"></script></head><body>')
+out_file.write(plot)
+out_file.write('</body><html>')
+out_file.close()
+
+
+# In[19]:
 
 
 # Plot
@@ -581,7 +653,7 @@ for i, c in enumerate(top10_country):
     
     lay = go.Layout(width = 600,
                     height = 500,
-                    xaxis = dict(nticks = 10,
+                    xaxis = dict(#nticks = 10,
                                  ticks = 'inside',
                                  ticklen = 12,
                                  tickcolor = '#eee',
@@ -603,7 +675,7 @@ for i, c in enumerate(top10_country):
                                   y = -0.1,
                                   orientation = 'h'
                                  ),
-                    margin=dict(l=50, r=20, b=100, t=50, pad=0),
+                    margin=dict(l=50, r=20, b=100, t=30, pad=0),
                )
           
                 
@@ -628,7 +700,7 @@ for i, c in enumerate(top10_country):
 #plotly.offline.iplot(fig)
 
 
-# In[22]:
+# In[20]:
 
 
 # Data to JSON
@@ -641,28 +713,22 @@ def up_or_down(x):
     else:
         return 'down'
 
-trend = tmp.apply(lambda x: up_or_down(x['confirmed_newcases_growth_movavg']), axis=1)
+tmp['trend'] = tmp.apply(lambda x: up_or_down(x['confirmed_newcases_growth_movavg']), axis=1)
+trend = tmp[['Country/Region', 'trend']].copy()
 
 most_recent_day = df_merged.sort_values('dt').dt.unique()[-1]
 tmp = df_merged[ (df_merged['dt'] == most_recent_day) & (df_merged['Country/Region'].isin(top10_country))].copy()
 tmp.sort_values(by = 'deaths', ascending = False, inplace = True)
 tmp = tmp[['Country/Region', 'days_since_1st_conf', 'first_confirmed', 
-           'confirmed', 'dt', 'confirmed_newcases']]
-tmp['trend'] = trend.values
+           'confirmed', 'deaths']]
+tmp = tmp.merge(trend, on = 'Country/Region')
 tmp['graph_number'] = np.arange(10)
+tmp['last_update'] = pd.Timestamp.now(tz='US/Eastern')
 tmp.set_index('Country/Region', inplace = True)
 tmp.rename(columns={'Country/Region': 'country',
                     'first_confirmed': 'date_first_confirmed',
-                    'dt': 'date',
                    }, inplace = True)
+path = '../visuals/new_cases/'
 tmp.to_json(path+'country_info.json', orient = 'columns')
+
 #tmp
-
-
-# In[23]:
-
-
-#The first case of COVID-19 in [country] was reported [number of days] days ago on [date]. 
-# Since then, the country has reported [case number] cases. 
-# On [date], the most recent reporting date, [country] reported [number of new cases] new cases.
-
